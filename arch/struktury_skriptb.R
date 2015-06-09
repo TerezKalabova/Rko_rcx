c59b32aca537fef647c70dc832dc7ef2f2f126a4
TerezKalabova <terez.kalabova@gmail.com>
2015-06-09 09:50:09 +0200

    deleni_retezce_v2

--- arch/struktury_skriptb.R
+++ arch/struktury_skriptb.R
@@ -24,10 +24,6 @@ NKB <- read.table("NKB3.txt", header=FALSE, sep=";")
 names(NKB) <- c("kod_ciselniku", "popis_otazky", "kody_K")
 arch.NKB<-NKB #zaloha pro jistotu. Znovunahrani: NKB<-arch.NKB
 
-#vsechny dotazniky / zkusebni podmnozina # TODO
-seznam_dotazniku<-c("P1", "P2", "P3", "N1", "N3", "N4", "N5", "PN6/1", "PN6/2", "PN6/3", "PN8", "PN18/1", "PN18/2", "PN18/3", "PN18/4", "T1", "T2", "T3", "T4", "T5",  "F1", "F2", "F3", "F4", "F5", "S1", "S2", "S3", "S4", "S5", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "Th4", "Ft1", "Ft2", "Ft3", "Ft4", "Ft5", "Ft6", "Ft7", "Ft8", "Et2", "Et4", "Et5", "Et6", "Et7", "Et8", "Et9", "V18", "Nt1", "Nt2", "Nt3", "Nt4", "Nt5", "Nt6", "Nt8", "Nt9", "V19", "U8", "U11", "U13", "U15", "U18", "V8", "V11", "V13", "V15", "FSS", "FSS8", "FSS11", "FSS13", "FSS15", "FSS17", "FSS19")
-
-
 #------------------------------------------------------------------------------------------------
 # uprava nactenych dat pro dalsi zpracovani
 
@@ -88,39 +84,12 @@ typy_skal<-c(rep("cisla",49), rep("rim", 49), rep("pis", 26))
 punct_pattern<- "#|%|=|~|@|#|_|:|<|>|;|'|&"   # dalsi nefunguji ("~"|"!"|"@"|"#"|"$"|"%"|"^"|"&"|"*"|"("|")"|"{"|"}"|"_"|"+"|":"|"<"|">"|"?"|"."|";"|"'"|"["|"]"|"=") obzvlast pat_zle<-"*, $, ?, ^, +."
 
 #predpriprava dat sloupce kodu_K
-NKB$kody_K<-gsub("[[:space:]]", " ", NKB$kody_K)              #nahrazeni vsech typu mezer jednotnou mezerou 1./3 krok
-NKB$kody_K<-gsub("\xc2\xa0", " ", NKB$kody_K)                 #nahrazeni vsech typu mezer jednotnou mezerou 2.3 krok
-NKB$kody_K<-gsub(" ", " ", NKB$kody_K)                        #nahrazeni vsech typu mezer jednotnou mezerou 3/3 krok
-NKB$kody_K<-gsub("_", " ", NKB$kody_K)                        #nahrazeni podtrzitka jednotnou mezerou
-NKB$kody_K<-gsub("v", " v ", NKB$kody_K)                      #vlozeni mezer kolem vsech "v"
-NKB$kody_K<-gsub("v i", "vi", NKB$kody_K)                     #odebrani mezery u "v" v rim. cisle
-NKB$kody_K<-gsub("x v", "xv", NKB$kody_K) 
-NKB$kody_K<-gsub("v 1", "v1", NKB$kody_K)                     #odebrani mezery u "v" pred cislem
-NKB$kody_K<-gsub("v 2", "v2", NKB$kody_K)         
-NKB$kody_K<-gsub("v 3", "v3", NKB$kody_K)                     
-NKB$kody_K<-gsub("v 4", "v4", NKB$kody_K)                     
-NKB$kody_K<-gsub("v 5", "v5", NKB$kody_K)                     
-NKB$kody_K<-gsub("v 6", "v6", NKB$kody_K)                     
-NKB$kody_K<-gsub("v 7", "v7", NKB$kody_K)                     
-NKB$kody_K<-gsub("v 8", "v8", NKB$kody_K)                     
-NKB$kody_K<-gsub("v 9", "v9", NKB$kody_K)      
-
-NKB$kody_K<-gsub("v 1", "v1", NKB$kody_K) 
-NKB$kody_K<-gsub("v 2", "v2", NKB$kody_K) 
-NKB$kody_K<-gsub("v 3", "v3", NKB$kody_K) 
-NKB$kody_K<-gsub("v 4", "v4", NKB$kody_K) 
-NKB$kody_K<-gsub("v 5", "v5", NKB$kody_K) 
-NKB$kody_K<-gsub("v 6", "v6", NKB$kody_K) 
-NKB$kody_K<-gsub("v 7", "v7", NKB$kody_K) 
-NKB$kody_K<-gsub("v 8", "v8", NKB$kody_K) 
-NKB$kody_K<-gsub("v 9", "v9", NKB$kody_K) 
-
+NKB$kody_K<-gsub("[[:space:]]", " ", NKB$kody_K)              #nahrazeni vsech typu mezer jednotno mezerou 1./3 krok
+NKB$kody_K<-gsub("\xc2\xa0", " ", NKB$kody_K)                 #nahrazeni vsech typu mezer jednotno mezerou 2.3 krok
+NKB$kody_K<-gsub(" ", " ", NKB$kody_K)                        #nahrazeni vsech typu mezer jednotno mezerou 3/3 krok
 NKB$kody_K<-(gsub("^ *|(?<= ) | *$", "", NKB$kody_K, perl=T)) #nahrazeni nasobnych mezer jednou mezerou
 NKB$kody_K<-gsub("–", "-", NKB$kody_K)                        #nahrazeni spojovniku pomlckou
-NKB$kody_K<-gsub("-", "-", NKB$kody_K)                        #nahrazeni divne pomlcky normalni pomlckou snad
-#NKB$kody_K<-gsub("- v v ", "-v v ", NKB$kody_K)               #odebrani mezery v retezci "- v v "
-NKB$kody_K<-gsub("- ", "-", NKB$kody_K)                       #odebrani mezer kolem pomlcky
-NKB$kody_K<-gsub(" -", "-", NKB$kody_K)                       #odebrani mezer kolem pomlcky
+NKB$kody_K<-gsub("- v v ", "-v v ", NKB$kody_K)               #odebrani mezery v retezci "- v v "
   #nahrazeni mezery v casti retezce "pismeno-mezera-cislo" (kod dotazniku, nejen)
  
 # prazdna tabulka _NK ciselnikuu s chybou v kodu_K
@@ -128,48 +97,45 @@ NK_cis_chyba_kody_K<-c()
 
 # seznam typu chyb
 pra_ret<-"Prazdny retezec"
-punct<-"punct #%=~@#_:<>;'&"
-bez_v<-"chybi delitko otazka-dotaznik"
-male_pis<-"male_pismeno_na_zac_otazky"
-velke_V<-"velke V na >2. miste otazky"
+punct<-"Punctuation characters"
+bez_v<-"chybi delitko parujici otazka-dotaznik"
 
 
 
 for (cis in 1:nrow(NKB)) { #prace po radcich K koduu
   ciselnik<-as.character(NKB$kod_ciselniku[cis])
   kody_K<-as.vector(NKB$kody_K[cis]) #jeden radek vsech kodu K
-  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # kontrola chyb v kody_K, pokud maji evidentne nekompatibilni tvar
   
  #chyby v kody_K, kdy se dale kody_K ani nezkousi analyzovat
   if (is.na(kody_K)) {  #prazdny retezec --> chyba
     #zapise cely chybny radek z NKB do tabulky chyb
-      NK_cis_chyba_kody_K<-rbind(NK_cis_chyba_kody_K , cbind(NKB[cis,], chyba=pra_ret))
-      #vypise chybu do listu cis a skace na dalsi kolo forcyklu
-      list_K<-as.data.frame(cbind(pra_ret, "chyba", "chyba"))    
+    NK_cis_chyba_kody_K<-rbind(NK_cis_chyba_kody_K , cbind(NKB[cis,], chyba=pra_ret))
+    #vypise chybu do listu cis a skace na dalsi kolo forcyklu
+      # TODO
     
   } else if (grepl(punct_pattern, kody_K)) { #nektere specialni znaky --> chyba "#|%|=|~|@|#|_|:|<|>|;|'|&"
     #pokud v kody_K je ne ktery divne znaky: !"#$%&'()*+.:;<=>?@[]^_`{|}~. vyhazuju chybu
       #zapise cely chybny radek z NKB do tabulky chyb
       NK_cis_chyba_kody_K<-rbind(NK_cis_chyba_kody_K , cbind(NKB[cis,], chyba=punct))
       #vypise chybu do listu cis a skace na dalsi kolo forcyklu
-      list_K<-as.data.frame(cbind(punct, "chyba", "chyba"))  
-            
+        # TODO
+      
   }else if (!grepl("v", kody_K)) { #kody_K neobsahuje zadne "v" --> chyba
     #zapise cely chybny radek z NKB do tabulky chyb
     NK_cis_chyba_kody_K<-rbind(NK_cis_chyba_kody_K , cbind(NKB[cis,], chyba=bez_v))
     #vypise chybu do listu cis a skace na dalsi kolo forcyklu
-    list_K<-as.data.frame(cbind(bez_v, "chyba", "chyba")) 
-    
-    
+        # TODO
     
   }else{ #kody_K obsahuje asi aspon jeden validni retezec --> ma smysl analyzovat kody_K
     
   # -----------------------------------------------------------------------------------------------
     # docasny pomocny usek, pak to prestehuju TODO
     
-             
+    #vsechny dotazniky / zkusebni podmnozina # TODO
+    seznam_dotazniku<-c("E5", "S5", "F5", "T5", "PN18/2", "T2", "F6", "G8")
+         
     #priklad kody_K: 
     # "E 3 v E 5, E 4 v S 5, F 4 v F 5, F 4 v T 5,O 4 v PN 18/2, I 4 v T 2"
     # kody_K<-"A 12bi-v v E 5, A 11i +A 33b1-5 v S 5, A 34b 1-5 v F 5, A 14a1-5 v T 5" 
@@ -180,7 +146,8 @@ for (cis in 1:nrow(NKB)) { #prace po radcich K koduu
     # krajeni 1 kodu K na retezce
           
       #rozdeleni kody_K na useky po vsech carkach
-      useky_mezi_carkami<-unlist(strsplit(kody_K, ","))        
+      useky_mezi_carkami<-unlist(strsplit(kody_K, ","))       
+      useky_pocty_znaku_bez_mezer<-gsub(" ","",useky_mezi_carkami)    
       pocet_useku<-length(useky_mezi_carkami)
   
       #slepeni useku kratsich nez 5
@@ -188,48 +155,68 @@ for (cis in 1:nrow(NKB)) { #prace po radcich K koduu
       nadejny_pahyl<-c()
       useky_predb_ok<-c()
       pahyl_otazek<-c()
-      for (us in 1 : length(useky_mezi_carkami) ){ #prochazeni kody_K po usecich
+      for (us in 1 : length(useky_mezi_carkami) ) { #prochazeni kody_K po usecich
         
         if (nchar(gsub(" ","", useky_mezi_carkami[us]))>=5 && grepl(" v ", useky_mezi_carkami[us])) { # usek ma min delku && obsahuje " v "
-          nadejny_pahyl<-gsub(" v "," @ ", useky_mezi_carkami[us] ) #@@@
-          nadej_pahyl_ret<-unlist(strsplit(nadejny_pahyl, " @ ")) #@@@
+          nadejny_pahyl<-useky_mezi_carkami[us]
+          nadej_pahyl_ret<-unlist(strsplit(nadejny_pahyl, " v "))
           if (length(nadej_pahyl_ret)==2 &&  is.element(gsub(" ","", nadej_pahyl_ret[2]), seznam_dotazniku) &&  !is.element(gsub(" ","", nadej_pahyl_ret[1]), prvky_skal) ) { # nadejny pahyl se rozpada prave na dve casti  && v nadejnem pahylu to za " v " ma typ seznam_dotazniku, to pred v neni prvek_skal
             useky_predb_ok<-c(useky_predb_ok, paste(pahyl_otazek, nadejny_pahyl, sep="")) #predbezne ulozeno do fin. useku
             pahyl_otazek<-c() #po vlozeni pahylu do vysledneho retezce ho opet vyprazdnim
-          } else if (length(nadej_pahyl_ret)==2 &&  is.element(gsub(" ","", nadej_pahyl_ret[2]), seznam_dotazniku) &&  is.element(gsub(" ","", nadej_pahyl_ret[1]), prvky_skal) ) { #nadejny pahyl se rozpada prave na casti: moznost skal - " v " - dotaznik. ->prilozi se k pahylu otazek a ulozi do useky_predb_ok
-            #test: if pahyl otazek je prazdny, hod chybu TODO. Zatim predpokladam ze nikdy neni prazdny.
-            useky_predb_ok<-c(useky_predb_ok, paste(pahyl_otazek, gsub(" v "," @ ",useky_mezi_carkami[us]), sep="") ) ##@@@
-            pahyl_otazek<-c() #po vlozeni pahylu do vysledneho retezce ho opet vyprazdnim ##
-          }else {
+          } else {
             #je to asi koncovy pahyl retezce zacinajici moznosti skaly, s vetsi delkou
-            pahyl_otazek<-gsub(" ","", paste(pahyl_otazek, useky_mezi_carkami[us],", ", sep=""))
+            pahyl_otazek<-paste(pahyl_otazek, useky_mezi_carkami[us], ", ", sep="")
             #tady by asi mela byt kontrola jestli uz je pahyl otazek v dostatecnem formatu a pripadne ho vlozit do useky_predb_ok
           }
         } else if  (!grepl(" v ", useky_mezi_carkami[us]) && !is.element(gsub(" ","", useky_mezi_carkami[us]), seznam_dotazniku) ) { # (pahyl otazek) neobsahuje " v " && neni dotaznik
-          pahyl_otazek<-gsub(" ","", paste(pahyl_otazek, useky_mezi_carkami[us], ",", sep=""))
+          pahyl_otazek<-paste(pahyl_otazek, useky_mezi_carkami[us], ", ", sep="")
         } else if (!grepl(" v ", useky_mezi_carkami[us]) && is.element(gsub(" ","", useky_mezi_carkami[us]), seznam_dotazniku) && (length(useky_predb_ok) >=1) ) { # (pahyl dotaznik) neobsahuje " v " && je na seznamu dotazniku && uz predtim byl aspon jeden usek ulozen
           # doplneni dotazniku k predchozi otazce
-          useky_predb_ok[length(useky_predb_ok)] <- paste(useky_predb_ok[length(useky_predb_ok)], ",", useky_mezi_carkami[us], sep="")
-        } else if ( nchar(gsub(" ","", useky_mezi_carkami[us]))==4 && grepl(" v ", useky_mezi_carkami[us]) && is.element(substr(useky_mezi_carkami[us],1,1),prvky_skal) && is.element( str_sub ( gsub(" ","", useky_mezi_carkami[us]), -2,-1 ) , seznam_dotazniku)) { #ma to jen 4 znaky, sklada se to z casti: prvek_skal-" v "-dotaznik /vsechny tri casti testovany
-          useky_predb_ok<-c(useky_predb_ok, paste(pahyl_otazek, gsub(" v "," @ ",useky_mezi_carkami[us]), sep=""))## @@@
-          pahyl_otazek<-c() #po vlozeni pahylu do vysledneho retezce ho opet vyprazdnim ##
+          useky_predb_ok[length(useky_predb_ok)] <- paste(useky_predb_ok[length(useky_predb_ok)], ", ", useky_mezi_carkami[us], sep="")
+        } else if (nchar(gsub(" ","", useky_mezi_carkami[us]))==4 && grepl(" v ", useky_mezi_carkami[us]) && is.element(substr(useky_mezi_carkami[us],1,1),prvky_skal) ) { #ma to jen 4 znaky, slkada se to z casti: prvek_skal-" v "-dotaznik
+          #tady by se mely odrezat vsude mezery v ifu TODO
           
+          #tady to ted hori!!!!! proto to nefunguje. cus v pondeli!
+          a<-1
         } else { # TODO odstranit/ jen pro vyvoj ukazani chyby
           useky_predb_ok<-c(useky_predb_ok, "chyba nevim kde")
         }
-        useky_ok<- useky_predb_ok #pokud je to ok
+        useky_ok<-c(useky_ok, useky_predb_ok) #pokud je to ok
       }#for u
 
+      #osetrit pripady:
+      # a<-"a v v d"
+          
           
 
         
+        
+        
+        
+        
+        
+        
+        
+        
+        #nejjednodussi funkcni pripad
+        if all(str_count(useky_mezi_carkami, " v ") == rep(1, length(useky_mezi_carkami)) ) {
+          je to vsechno asi ok
+        }
+        
+      
+        
+        
+        
+        
+        
+        
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        K_po_retezcich<-useky_ok    ###8.6.2015 || unlist(strsplit(kody_K, ", ")) #rozdeleni K kodu do retezcu po dotaznicich podle ", "
+        K_po_retezcich<-unlist(strsplit(kody_K, ", ")) #rozdeleni K kodu do retezcu po dotaznicich podle ", "
         
         otazky<-dotazniky<-otazkyDB<-c() #vysledne otazky jednoho ciselniku a 1 kodu_K, vkladaji se do 1 listu s nazvem ciselniku
         for (dot in 1: length(K_po_retezcich) ) { #prace po retezcich z kodu K
           #krajeni retezce kodu K na OTAZKA - DOTAZNIK 
-          ret<-gsub(" ", "", unlist(strsplit(K_po_retezcich[dot], " @ "))) #rozdeleni K kodu na otazku a dotaznik podle " v " @@@
+          ret<-gsub(" ", "", unlist(strsplit(K_po_retezcich[dot], " v "))) #rozdeleni K kodu na otazku a dotaznik podle " v "
           OTAZKA<-ret[1]
           DOTAZNIK<-ret[2]
           
@@ -339,26 +326,10 @@ for (cis in 1:nrow(NKB)) { #prace po radcich K koduu
           #otazkyDB<-c(otazkyDB,komb_DOTAZNIK_OTAZKA)
         }#for dot K_po_retezcich (retezcich)
         
-        #kontrola velkeho pismena na zacatku otazky
-        if (any(str_sub(otazky, 1, 1) != toupper(str_sub(otazky, 1, 1)))) { #odchyceni otazek zacinajich malym pismenem (nejspis spatne rozdelene)
-          #zapise cely chybny radek z NKB do tabulky chyb
-          NK_cis_chyba_kody_K<-rbind(NK_cis_chyba_kody_K , cbind(NKB[cis,], chyba=male_pis))
-          #vypise chybu do listu cis a skace na dalsi kolo forcyklu
-          list_K<-as.data.frame(cbind(male_pis, "chyba", "chyba"))
-        } else if (any(grepl("V", str_sub(gsub(" ","",otazky), 2)))) { #odchyceni otazek s velkym V na jinem ne prvnim miste a proto spatne delenych
-          #zapise cely chybny radek z NKB do tabulky chyb
-          NK_cis_chyba_kody_K<-rbind(NK_cis_chyba_kody_K , cbind(NKB[cis,], chyba=velke_V))
-          #vypise chybu do listu cis a skace na dalsi kolo forcyklu
-          list_K<-as.data.frame(cbind(velke_V, "chyba", "chyba"))
-        } else{
-          
-          
         otazkyDB<-paste(dotazniky, otazky,  sep="_")
-        list_K<-as.data.frame(cbind(otazky, dotazniky, otazkyDB))  
-        
-        }
   } #else - kontrola celkovych chyb v kody_K
-
+ 
+  list_K<-as.data.frame(cbind(otazky, dotazniky, otazkyDB))
   NK2[[cis]]<-list_K
 }#for cis NK2
 names(NK2)<-NKB$kod_ciselniku
